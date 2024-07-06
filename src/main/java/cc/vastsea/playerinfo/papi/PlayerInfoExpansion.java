8c6f25483f90c0de0debe2dcc13d3df9a947f4ee
Snowball_233 <wangrb.qq@outlook.com>
2024-07-06 22:00:40 +0800

    Add three independent features

--- src/main/java/cc/vastsea/playerinfo/papi/PlayerInfoExpansion.java
+++ src/main/java/cc/vastsea/playerinfo/papi/PlayerInfoExpansion.java
@@ -10,13 +10,13 @@ import org.geysermc.floodgate.api.player.FloodgatePlayer;
 
 /**
  * PlaceholderAPI expansion for PlayerInfo.
- * @version 1.0.0
+ * @version 1.1
  * @author Snowball_233
  * @see PlaceholderExpansion
  */
 public class PlayerInfoExpansion extends PlaceholderExpansion {
 
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.1";
 
     @Override
     public boolean persist() {
@@ -36,7 +36,7 @@ public class PlayerInfoExpansion extends PlaceholderExpansion {
 
     @Override
     public String getIdentifier() {
-        return "player";
+        return "playerinfo";
     }
 
     @Override
@@ -50,14 +50,25 @@ public class PlayerInfoExpansion extends PlaceholderExpansion {
             return "";
         }
 
-        if (identifier.equalsIgnoreCase("info")) {
-            String version = getPlayerVersion(player);
-            String platform = getPlayerPlatformString(player);
-            String device = getPlayerDeviceString(player);
-            return String.format("[%s][%s][%s]", version, platform, device);
-        }
+        switch (identifier.toLowerCase()) {
+            case "player_info":
+                String version = getPlayerVersion(player);
+                String platform = getPlayerPlatformString(player);
+                String device = getPlayerDeviceString(player);
+                return String.format("[%s][%s][%s]", version, platform, device);
 
-        return null;
+            case "player_version":
+                return getPlayerVersion(player);
+
+            case "player_platform":
+                return getPlayerPlatformString(player);
+
+            case "player_device":
+                return getPlayerDeviceString(player);
+
+            default:
+                return null;
+        }
     }
 
     private String getPlayerVersion(Player player) {
